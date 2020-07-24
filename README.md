db2320fd22c1a3477fa90729a18128c4b751c858
glorphindale <glorphindale@gmail.com>
2020-07-24 19:36:11 +0700

    day2: Add box movement.

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Manipulate canvas with JS interop.
 Issues: clj workflow is very unstable, `(require '[parenslike.game :as game] :reload)` constantly hangs.
 
 Peculiarities: reloading index.html results in 5Mb of data to be transferred.
+
+### Day 2 (24.07.2020)
+
+Added box movement - keypress listeners, rendering loop.
+
+Issues: don't understand how reloading code from REPL works - sometimes functions are updated, sometimes not. Hard to understand how to properly interop with JS libraries.
--- src/parenslike/game.cljs
+++ src/parenslike/game.cljs
@@ -1,10 +1,45 @@
-(ns parenslike.game)
+(ns parenslike.game
+  (:require [goog.events :as g-events]))
 
-(let [width 600
-      height 600
-      canvas (. js/document querySelector "canvas")]
-  (do (set! (.-width canvas) width)
-      (set! ( .-height canvas) height)
-      (set! (-> canvas .-width .-style) (str width "px"))
-      (set! (-> canvas .-height .-style) (str height "px"))
-      ))
+(def height 600)
+(def width 600)
+(def tile-size 40)
+
+(defonce state (atom {:init false :position {:x 0 :y 0}}))
+
+(defn keypress [event]
+  (do
+    (case (.-key event)
+      "w"  (swap! state update-in [:position :y] dec)
+      "s"  (swap! state update-in [:position :y] inc)
+      "a"  (swap! state update-in [:position :x] dec)
+      "d"  (swap! state update-in [:position :x] inc))
+))
+
+(defn draw []
+  (let [canvas (. js/document querySelector "canvas")
+        ctx (. canvas getContext "2d")
+        position (get-in @state [:position])]
+    (do
+      (. ctx clearRect 0 0 width height)
+      (. ctx fillRect (* tile-size (:x position)) (* tile-size (:y position)) tile-size tile-size)
+      )))
+
+
+(defn init []
+  (let [canvas (. js/document querySelector "canvas")]
+    (do (set! (.-width canvas) width)
+        (set! (.-height canvas) height)
+        (set! (-> canvas .-width .-style) (str width "px"))
+        (set! (-> canvas .-height .-style) (str height "px"))
+        (. js/window setInterval draw 15)
+        (.addEventListener js/document "keypress" keypress)
+        (swap! state assoc-in [:init] true)
+        )))
+
+(js/console.log "Loading code")
+
+(when (not (@state :init))
+  (do
+    (js/console.log "Initial load")
+    (init)))
